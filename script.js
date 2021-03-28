2f13e4943696d4cbcb1370deecffbae21bbfdd36
Twinkle gupta <gtwinkle132@gmail.com>
2021-03-28 20:41:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const rollno = document.getElementById('rollno');
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click',generatelist);
+
+function generatelist(e)
+{
+    console.log(rollno.value);
+    var arr=[];
+    
+}
